820d211595d56064a067fb9e2edc4629ed685dc6
younes <younes3021.us@gmail.com>
2025-01-11 03:05:47 +0330

    problems fixed

--- spell-checker/spellChecker.py
+++ spell-checker/spellChecker.py
@@ -13,24 +13,23 @@ def check_spelling():
     a = TextBlob(word)
     right = str(a.correct())
 
-    cs = Label(root, text="Correct text is:", font=('poppins', 20), bg="dae6f6", fg="364971")
+    cs = Label(root, text="Correct text is:", font=('poppins', 20), bg="lightblue", fg="black")
     cs.place(x=100, y=250)
     spell.config(text=right)
 
 
-heading = Label(root, text="Spelling Checker", font=("Trebuchet MS", 30, "bold"), bg="dae6f6", fg='#364971')
+heading = Label(root, text="Spelling Checker", font=("Trebuchet MS", 30, "bold"), bg="white", fg='black')
 heading.pack(pady=(50, 0))
 
-enter_text = Entry(root, justify='center', width=30, font=('poppins', 25), bg='white', border=2)
+enter_text = Entry(root, justify='center', width=30, font=('poppins', 25), bg='lightblue', border=2)
 enter_text.pack(pady=10)
 enter_text.focus()
 
 button = Button(root, text="Check", font=('arial', 20, 'bold'), fg='white', bg='red', command=check_spelling)
 button.pack()
 
-spell = Label(root, font=('poppins', 20), bg="#daef6", fg="364971")
+spell = Label(root, font=('poppins', 20), bg="lightblue", fg="black")
 spell.place(x=350, y=250)
 
 
 root.mainloop()
-
